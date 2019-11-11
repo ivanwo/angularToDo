0c529b763ae14156610890da5c581ef270bf306d
ivan <ivanwolyniec@gmail.com>
2019-11-11 16:30:15 -0500

    fixed some features and cleaned up code

--- src/app/to-do/to-do.component.ts
+++ src/app/to-do/to-do.component.ts
@@ -22,6 +22,7 @@ export class ToDoComponent implements OnInit {
   constructor() {}
 
   tempTask: string;
+  sortInput: string;
   list: ToDo[] = [
     { task: "fold clothes", completed: false },
     { task: "put clothes in dresser", completed: true },
@@ -31,9 +32,54 @@ export class ToDoComponent implements OnInit {
     { task: "be awesome", completed: false },
     { task: "one more thing", completed: false }
   ];
+  sortedList: ToDo[] = this.list;
   adder(e) {
     this.list.push({ task: e, completed: false });
     this.tempTask = "";
+  }
+  sorter() {
+    this.sortedList = [];
+    if (this.sortInput === "") {
+      for (let todo of this.list) {
+        this.sortedList.push({ task: todo.task, completed: todo.completed });
+      }
+    } else {
+      for (let i = 0; i < this.list.length; i++) {
+        if (this.list[i].task.includes(this.sortInput)) {
+          this.sortedList.push({
+            task: this.list[i].task,
+            completed: this.list[i].completed
+          });
+        }
+      }
+    }
+  }
+  editor(task: string) {
+    // let newContent:string = prompt("new task content: ");
+    for (let i = 0; i < this.list.length; i++) {
+      if (this.list[i].task === task) {
+        this.list[i].task = prompt("new task content: ");
+      }
+    }
+    if (this.sortInput !== "") this.sorter();
+  }
+  clearSort() {
+    this.sortInput = "";
+    this.sorter();
+  }
+  delete(task: string) {
+    // alert(task);
+    let index: number;
+    for (let i = 0; i < this.list.length; i++) {
+      if (this.list[i].task === task) {
+        index = i;
+      }
+    }
+    if (index >= 0) {
+      this.list.splice(index, 1);
+      if (!this.sortInput) this.sortInput = "";
+      this.sorter();
+    }
   }
   ngOnInit() {}
 }
